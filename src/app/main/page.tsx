dbeb506acad45b0eec9fe5a934608efe14f1f663
SSkkky <worte5633@gmail.com>
2024-03-25 09:53:04 +0900

    merge

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 import TextLogo from '@/app/assets/logo/textLogo.png';
 import Opening from '@/app/components/Opening';
 
-export default function Home() {
-  return (
-    <>
-      <Opening />
-    </>
-  );
+export default function Main() {
+    return (
+        <>
+            <p>메인</p>
+        </>
+    );
 }
